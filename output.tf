4a47eadbfd1c3513240a6a539684f8b9389eb4e3
chlals096 <136772261+chlals096@users.noreply.github.com>
2023-10-31 11:25:18 +0900

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,9 @@
+
+output "sg_id" {
+  value = aws_security_group.ssh_sg.id
+}
+
+
+output "sg_name" {
+  value = aws_security_group.ssh_sg.name
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_id" {
+  type = string
+  description = "보안그룹의 vpc ID"
+  
+}
